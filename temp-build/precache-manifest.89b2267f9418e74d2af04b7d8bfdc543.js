6d4d4ed9510a5334d7c93f768a10dc8ffe49cc8c
Stefan <stefanroetcode@gmail.com>
2026-02-03 21:37:43 +0200

    done 4 now

---
+++ temp-build/precache-manifest.89b2267f9418e74d2af04b7d8bfdc543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb7b7720aed2e999847344ccf3ebe5e",
+    "url": "/Stefans-Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "c50b9ebd3fcf4517be7e",
+    "url": "/Stefans-Tic-Tac-Toe/static/css/main.8591d698.chunk.css"
+  },
+  {
+    "revision": "615b66612b5d36aaac78",
+    "url": "/Stefans-Tic-Tac-Toe/static/js/2.9e23dbca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Stefans-Tic-Tac-Toe/static/js/2.9e23dbca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50b9ebd3fcf4517be7e",
+    "url": "/Stefans-Tic-Tac-Toe/static/js/main.3045b42f.chunk.js"
+  },
+  {
+    "revision": "516f8eff6001bd5d4bd2",
+    "url": "/Stefans-Tic-Tac-Toe/static/js/runtime-main.edffbc66.js"
+  }
+]);
